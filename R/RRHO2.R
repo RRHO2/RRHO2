a594f9dbfeea200ae1e0c640c30d474eca172df4
KellyCahill <kmc152@pitt.edu>
2017-07-24 10:50:23 -0400

    Revert "Fisher"

--- R/RRHO2.R
+++ R/RRHO2.R
@@ -33,7 +33,7 @@
 
 RRHO2 <- function (list1, list2, stepsize = defaultStepSize(list1, list2),
           labels, plots = FALSE, outputdir = NULL, BY = FALSE,
-          log10.ind = FALSE, maximum=200, boundary = 0.1, res=30, method="fisher")
+          log10.ind = FALSE, maximum=200, boundary = 0.1, res=30)
 {
   if (length(list1[, 1]) != length(unique(list1[, 1])))
     stop("Non-unique gene identifier found in list1")
@@ -52,9 +52,9 @@ RRHO2 <- function (list1, list2, stepsize = defaultStepSize(list1, list2),
   nlist2 <- length(list2[, 1])
 
   N <- max(nlist1, nlist2)
-  .hypermat_normal <- numericListOverlap(list1[, 1], list2[, 1], stepsize, method=method)
+  .hypermat_normal <- numericListOverlap(list1[, 1], list2[, 1], stepsize)
   hypermat_normal <- .hypermat_normal$log.pval
-  .hypermat_flipX <- numericListOverlap(rev(list1[, 1]), list2[, 1], stepsize, method=method)
+  .hypermat_flipX <- numericListOverlap(rev(list1[, 1]), list2[, 1], stepsize)
   hypermat_flipX <- .hypermat_flipX$log.pval
   hypermat_flipX2 <- hypermat_flipX[nrow(hypermat_flipX):1,]
 
@@ -93,7 +93,47 @@ RRHO2 <- function (list1, list2, stepsize = defaultStepSize(list1, list2),
     result$hypermat.by <- hypermat.by
   }
   
-   
+  
+  maxind.dd <- which(max(hypermat[lenStrip1 + (boundary1+1):len1, lenStrip2 + (boundary2+1):len2],
+                         na.rm = TRUE) == hypermat, arr.ind = TRUE)
+  #
+  maxind.dd <- maxind.dd[maxind.dd[,1]>=lenStrip1 + (boundary1+1) & maxind.dd[,1]<=lenStrip1 +len1 & 
+  							maxind.dd[,2]>=lenStrip2 + (boundary2+1) & maxind.dd[,2]<=lenStrip2 + len2,]
+
+  indlist1.dd <- seq(1, nlist1, stepsize)[maxind.dd[1] - lenStrip1]
+  indlist2.dd <- seq(1, nlist2, stepsize)[maxind.dd[2] - lenStrip2]
+  genelist.dd <- intersect(list1[indlist1.dd:nlist1,
+                                 1], list2[indlist2.dd:nlist2, 1])
+  maxind.uu <- which(max(hypermat[1:boundary1, 1:boundary2],
+                         na.rm = TRUE) == hypermat, arr.ind = TRUE)
+  #
+  maxind.uu <- maxind.uu[maxind.uu[,1]>=1 & maxind.uu[,1]<=boundary1 & maxind.uu[,2]>=1 & maxind.uu[,2]<=boundary2,]
+
+  indlist1.uu <- seq(1, nlist1, stepsize)[maxind.uu[1]]
+  indlist2.uu <- seq(1, nlist2, stepsize)[maxind.uu[2]]
+  genelist.uu <- intersect(list1[1:indlist1.uu, 1],
+                           list2[1:indlist2.uu, 1])
+  #
+  maxind.ud <- which(max(hypermat[1:boundary1, lenStrip2 + (boundary2+1):len2],
+                         na.rm = TRUE) == hypermat, arr.ind = TRUE)
+  #
+  maxind.ud <- maxind.ud[maxind.ud[,1]>=1 & maxind.ud[,1]<=boundary1 & maxind.ud[,2]>= lenStrip2 + (boundary2+1) & maxind.ud[,2]<=lenStrip2 + len2,]
+
+  indlist1.ud <- seq(1, nlist1, stepsize)[maxind.ud[1]]
+  indlist2.ud <- seq(1, nlist2, stepsize)[maxind.ud[2] - lenStrip2]
+  genelist.ud <- intersect(list1[1:indlist1.ud,
+                                 1], list2[indlist2.ud:nlist2, 1])
+  maxind.du <- which(max(hypermat[lenStrip1 + (boundary1+1):len1, 1:boundary2],
+                         na.rm = TRUE) == hypermat, arr.ind = TRUE)
+  #
+  maxind.du <- maxind.du[maxind.du[,1]>=lenStrip1 + (boundary1+1) & maxind.du[,1]<=lenStrip1 + len1 & maxind.du[,2]>=1 & maxind.du[,2]<=boundary2,]
+
+  indlist1.du <- seq(1, nlist1, stepsize)[maxind.du[1] - lenStrip1]
+  indlist2.du <- seq(1, nlist2, stepsize)[maxind.du[2]]
+  genelist.du <- intersect(list1[indlist1.du:nlist1, 1],
+                           list2[1:indlist2.du, 1])
+
+  
   if (plots) {
     try({
       color.bar <- function(lut, min, max = -min, nticks = 11,
@@ -110,6 +150,77 @@ RRHO2 <- function (list1, list2, stepsize = defaultStepSize(list1, list2),
       }
 	  
 	  
+	  ################
+	  ## with highest point
+	  ################
+	  
+      .filename <- paste("RRHOMap_markH_combined_", labels[1], "_VS_",
+                         labels[2], ".tiff", sep = "")
+      tiff(filename = paste(outputdir, .filename, sep = "/"),
+           width = 8, height = 8, units = "in", 
+           res = res)
+      jet.colors <- colorRampPalette(c("#00007F", "blue",
+                                       "#007FFF", "cyan", "#7FFF7F", "yellow", "#FF7F00",
+                                       "red", "#7F0000"))
+      layout(matrix(c(rep(1, 5), 2), 1, 6, byrow = TRUE))
+      image(hypermat, xlab = "", ylab = "", col = jet.colors(101),
+            axes = FALSE, main = "Rank Rank Hypergeometric Overlap Map")
+	  segments(x0 = boundary1/len1 ,x1 = boundary1 /len1 ,y0 = -0.2,y1 = 1.2,lwd=4,col='white')
+	  segments(x0 = -0.2,x1 = 1.2,y0 = boundary2/len2,y1 = boundary2/len2,lwd=4,col='white')	  
+	  x1.dd <- (maxind.dd[1] - 1)/(nrow(hypermat) - 1)
+	  x2.dd <- (maxind.dd[2] - 1)/(ncol(hypermat) - 1)
+	  points(x1.dd,x2.dd,pch=18,cex=4)	  	  
+	  x1.uu <- (maxind.uu[1] - 1)/(nrow(hypermat) - 1)
+	  x2.uu <- (maxind.uu[2] - 1)/(ncol(hypermat) - 1)
+	  points(x1.uu,x2.uu,pch=18,cex=4)	  	  
+	  x1.ud <- (maxind.ud[1] - 1)/(nrow(hypermat) - 1)
+	  x2.ud <- (maxind.ud[2] - 1)/(ncol(hypermat) - 1)
+	  points(x1.ud,x2.ud,pch=18,cex=4)	  	  
+	  x1.du <- (maxind.du[1] - 1)/(nrow(hypermat) - 1)
+	  x2.du <- (maxind.du[2] - 1)/(ncol(hypermat) - 1)
+	  points(x1.du,x2.du,pch=18,cex=4)	  	  
+	  
+      mtext(labels[2], 2, 0.5)
+      mtext(labels[1], 1, 0.5)
+	  
+      finite.ind <- is.finite(hypermat)
+      color.bar(jet.colors(101), min = min(hypermat[finite.ind],
+                                           na.rm = TRUE), max = max(hypermat[finite.ind],
+                                                                    na.rm = TRUE), nticks = 6, title = "-log(P-value)")
+      dev.off()
+
+ 	  
+	  ## maximum
+      .filename <- paste("RRHOMap_markH_fixMax_combined_", labels[1], "_VS_",
+                         labels[2], ".tiff", sep = "")
+      tiff(filename = paste(outputdir, .filename, sep = "/"),
+           width = 8, height = 8, units = "in", 
+           res = res)
+      jet.colors <- colorRampPalette(c("#00007F", "blue",
+                                       "#007FFF", "cyan", "#7FFF7F", "yellow", "#FF7F00",
+                                       "red", "#7F0000"))
+      layout(matrix(c(rep(1, 5), 2), 1, 6, byrow = TRUE))
+      image(hypermat, xlab = "", ylab = "", col = jet.colors(101),breaks=c(seq(0,maximum,length.out = 101),1e10),
+            axes = FALSE, main = "Rank Rank Hypergeometric Overlap Map")
+	  segments(x0 = boundary1/len1 ,x1 = boundary1 /len1 ,y0 = -0.2,y1 = 1.2,lwd=4,col='white')
+	  segments(x0 = -0.2,x1 = 1.2,y0 = boundary2/len2,y1 = boundary2/len2,lwd=4,col='white')	  
+	  x1.dd <- (maxind.dd[1] - 1)/(nrow(hypermat) - 1)
+	  x2.dd <- (maxind.dd[2] - 1)/(ncol(hypermat) - 1)
+	  points(x1.dd,x2.dd,pch=18,cex=4)	  	  
+	  x1.uu <- (maxind.uu[1] - 1)/(nrow(hypermat) - 1)
+	  x2.uu <- (maxind.uu[2] - 1)/(ncol(hypermat) - 1)
+	  points(x1.uu,x2.uu,pch=18,cex=4)	  	  
+	  x1.ud <- (maxind.ud[1] - 1)/(nrow(hypermat) - 1)
+	  x2.ud <- (maxind.ud[2] - 1)/(ncol(hypermat) - 1)
+	  points(x1.ud,x2.ud,pch=18,cex=4)	  	  
+	  x1.du <- (maxind.du[1] - 1)/(nrow(hypermat) - 1)
+	  x2.du <- (maxind.du[2] - 1)/(ncol(hypermat) - 1)
+	  points(x1.du,x2.du,pch=18,cex=4)	  	  
+      mtext(labels[2], 2, 0.5)
+      mtext(labels[1], 1, 0.5)
+      finite.ind <- is.finite(hypermat)
+      color.bar(jet.colors(101), min = 0, max = maximum, nticks = 6, title = "-log(P-value)")
+      dev.off()
 
   	  
 	  ################
@@ -136,8 +247,105 @@ RRHO2 <- function (list1, list2, stepsize = defaultStepSize(list1, list2),
       color.bar(jet.colors(101), min = min(hypermat[finite.ind],
                                            na.rm = TRUE), max = max(hypermat[finite.ind],
                                                                     na.rm = TRUE), nticks = 6, title = "-log(P-value)")
-      dev.off()   
+      dev.off()
+
+      
+	  ## maximum
+      .filename <- paste("RRHOMap_fixMax_combined_", labels[1], "_VS_",
+                         labels[2], ".tiff", sep = "")
+      tiff(filename = paste(outputdir, .filename, sep = "/"),
+           width = 8, height = 8, units = "in", 
+           res = res)
+      jet.colors <- colorRampPalette(c("#00007F", "blue",
+                                       "#007FFF", "cyan", "#7FFF7F", "yellow", "#FF7F00",
+                                       "red", "#7F0000"))
+      layout(matrix(c(rep(1, 5), 2), 1, 6, byrow = TRUE))
+      image(hypermat, xlab = "", ylab = "", col = jet.colors(101),breaks=c(seq(0,maximum,length.out = 101),1e10),
+            axes = FALSE, main = "Rank Rank Hypergeometric Overlap Map")
+	  segments(x0 = boundary1/len1 ,x1 = boundary1 /len1 ,y0 = -0.2,y1 = 1.2,lwd=4,col='white')
+	  segments(x0 = -0.2,x1 = 1.2,y0 = boundary2/len2,y1 = boundary2/len2,lwd=4,col='white')	  
+      mtext(labels[2], 2, 0.5)
+      mtext(labels[1], 1, 0.5)
+      finite.ind <- is.finite(hypermat)
+      color.bar(jet.colors(101), min = 0, max = maximum, nticks = 6, title = "-log(P-value)")
+      dev.off()
+  
+	  	  
+      .filename <- paste(outputdir, "/RRHO_down_",
+                         labels[1], "_VS_down_", labels[2], ".csv", sep = "")
+      write.table(genelist.dd, .filename, row.names = F,
+                  quote = F, col.names = F)
+      .filename <- paste(outputdir, "/RRHO_up_",
+                         labels[1], "_VS_up_", labels[2], ".csv", sep = "")
+      write.table(genelist.uu, .filename, row.names = F,
+                  quote = F, col.names = F)
+      .filename <- paste(outputdir, "/RRHO_VennCon", labels[1],
+                         "_VS_", labels[2], ".tiff", sep = "")
+      tiff(.filename, width = 8.5, height = 5, units = "in",
+            res = res)
+      vp1 <- viewport(x = 0.25, y = 0.5, width = 0.5, height = 0.9)
+      vp2 <- viewport(x = 0.75, y = 0.5, width = 0.5, height = 0.9)
+      pushViewport(vp1)
+      h1 <- draw.pairwise.venn(length(indlist1.dd:nlist1),
+                               length(indlist2.dd:nlist2), length(genelist.dd),
+                               category = c(labels[1], labels[2]), scaled = TRUE,
+                               lwd = c(0, 0), fill = c("cornflowerblue", "darkorchid1"),
+                               cex = 1, cat.cex = 1.2, cat.pos = c(0, 0), ext.text = FALSE,
+                               ind = FALSE, cat.dist = 0.01)
+      grid.draw(h1)
+      grid.text(paste("Down",labels[1],"Down",labels[2]), y = 1)
+      upViewport()
+      pushViewport(vp2)
+      h2 <- draw.pairwise.venn(length(1:indlist1.uu), length(1:indlist2.uu),
+                               length(genelist.uu), category = c(labels[1],
+                                                                 labels[2]), scaled = TRUE, lwd = c(0, 0), fill = c("cornflowerblue",
+                                                                                                                    "darkorchid1"), cex = 1, cat.cex = 1.2, cat.pos = c(0,
+                                                                                                                                                                        0), ext.text = FALSE, main = "Negative", ind = FALSE,
+                               cat.dist = 0.01)
+      grid.draw(h2)
+      grid.text(paste("Up",labels[1],"Up",labels[2]), y = 1)
+      dev.off()
+
+      .filename <- paste(outputdir, "/RRHO_down_",
+                         labels[1], "_VS_up_", labels[2], ".csv", sep = "")
+      write.table(genelist.du, .filename, row.names = F,
+                  quote = F, col.names = F)
+      .filename <- paste(outputdir, "/RRHO_up_",
+                         labels[1], "_VS_down_", labels[2], ".csv", sep = "")
+      write.table(genelist.ud, .filename, row.names = F,
+                  quote = F, col.names = F)
+	  #
+      .filename <- paste(outputdir, "/RRHO_VennDis", labels[1],
+                         "_VS_", labels[2], ".tiff", sep = "")
+      tiff(.filename, width = 8.5, height = 5, units = "in",
+            res = res)
+      vp1 <- viewport(x = 0.25, y = 0.5, width = 0.5, height = 0.9)
+      vp2 <- viewport(x = 0.75, y = 0.5, width = 0.5, height = 0.9)
+      pushViewport(vp1)
+      h1 <- draw.pairwise.venn(length(indlist1.du:nlist1),
+                               length(1:indlist2.du), length(genelist.du),
+                               category = c(labels[1], labels[2]), scaled = TRUE,
+                               lwd = c(0, 0), fill = c("cornflowerblue", "darkorchid1"),
+                               cex = 1, cat.cex = 1.2, cat.pos = c(0, 0), ext.text = FALSE,
+                               ind = FALSE, cat.dist = 0.01)
+      grid.draw(h1)
+      grid.text(paste("Down",labels[1],"Up",labels[2]), y = 1)
+      upViewport()
+      pushViewport(vp2)
+      h2 <- draw.pairwise.venn(length(1:indlist1.ud), length(indlist2.ud:nlist2),
+                               length(genelist.ud), category = c(labels[1], labels[2]), scaled = TRUE,
+							   lwd = c(0, 0), fill = c("cornflowerblue", "darkorchid1"), cex = 1, cat.cex = 1.2, cat.pos = c(0, 0), ext.text = FALSE,
+							   main = "Negative", ind = FALSE,
+                               cat.dist = 0.01)
+      grid.draw(h2)
+      grid.text(paste("Up",labels[1],"Down",labels[2]), y = 1)
+      dev.off()
+
+
+
     })
+    if (length(h2) == 0L)
+      message("Unable to output JPG plots.")
   }
   result$hypermat <- hypermat
   return(result)
--- R/numericListOverlap.R
+++ R/numericListOverlap.R
@@ -1,44 +1,22 @@
 
 ## Compute the overlaps between two *numeric* lists:
-numericListOverlap<- function(sample1, sample2, stepsize, method="hyper"){
+numericListOverlap<- function(sample1, sample2, stepsize){
   n<- length(sample1)
   
-  overlap_hyper <- function(a,b) {
+  overlap<- function(a,b) {
     count<-as.integer(sum(as.numeric(sample1[1:a] %in% sample2[1:b])))    
-    log.pval<- -phyper(q=count-1, m=a, n=n-a+1, k=b, lower.tail=FALSE, log.p=TRUE)         
+	log.pval<- -phyper(q=count-1, m=a, n=n-a+1, k=b, lower.tail=FALSE, log.p=TRUE)         
     signs<- 1L
-    
+	    
     return(c(counts=count, 
              log.pval=as.numeric(log.pval),
-             signs=as.integer(signs)
-    ))    
+			 signs=as.integer(signs)
+             ))    
   }
   
-  overlap_fisher <- function(a,b) {
-    s1 <- sample1[1:a]
-    s2 <- sample2[1:b]
-    lenA <- as.integer(sum(as.numeric(s1 %in% s2))) 
-    lenB <- length(s1)
-    lenC <- length(s2)
-    
-    Odds <- lenA/(lenB-lenA)/(lenC-lenA)*(n*2 - lenB - lenC + lenA)
-    logOdds <- log(Odds)
-    logOdds[!is.finite(logOdds)] <- sign(logOdds[!is.finite(logOdds)]) * 100
-    signs<- 1L
-    
-    return(c(counts=lenA, 
-             log.pval=as.numeric(logOdds),
-             signs=as.integer(signs)
-    ))    
-  }
   
   indexes<- expand.grid(i=seq(1,n,by=stepsize), j=seq(1,n,by=stepsize))
-  if(method=="hyper"){
-    overlaps<- apply(indexes, 1, function(x) overlap_hyper(x['i'], x['j']))
-    
-  } else if(method=="fisher"){
-    overlaps<- apply(indexes, 1, function(x) overlap_fisher(x['i'], x['j']))
-  }
+  overlaps<- apply(indexes, 1, function(x) overlap(x['i'], x['j']))
   
   nrows<- sqrt(ncol(overlaps))
   matrix.counts<- matrix(overlaps['counts',], ncol=nrows)  
@@ -51,10 +29,11 @@ numericListOverlap<- function(sample1, sample2, stepsize, method="hyper"){
 # n<- 112
 # sample1<- sample(n)
 # sample2<- sample(n)  
-# .test<- numericListOverlap(sample1, sample2, 10, method="hyper")
-# .test<- numericListOverlap(sample1, sample2, 10, method="fisher")
+# .test<- RRHO:::numericListOverlap(sample1, sample2, 10)
 # dim(.test$log.oval)
 # library(lattice)
 # levelplot(.test$counts)
 # levelplot(.test$log.pval)
 # table(is.na(.test$log.pval))
+
+

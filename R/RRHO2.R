791b769044aedc294599a958f0d6206805f67a9f
KellyCahill <kmc152@pitt.edu>
2017-07-27 15:57:21 -0400

    Update RRHO2.R

--- R/RRHO2.R
+++ R/RRHO2.R
@@ -74,9 +74,14 @@ RRHO2 <- function (list1, list2, stepsize = defaultStepSize(list1, list2),
   hypermat <- matrix(NA,nrow=nrow(hypermat_normal) + lenStrip1,ncol=ncol(hypermat_normal) + lenStrip2)
   hypermat[1:boundary1,1:boundary2] <- hypermat_normal[1:boundary1,1:boundary2] ## u1u2, quadrant III
   hypermat[lenStrip1 + (boundary1+1):len1,lenStrip2 + (boundary2+1):len2] <- hypermat_normal[(boundary1+1):len1,(boundary2+1):len2] ## d1d2, quadrant I
+ 	max<-max(hypermat[is.finite(hypermat)])
+	hypermat[hypermat == Inf] <- max
   hypermat[1:boundary1,lenStrip2 + (boundary2+1):len2] <- hypermat_flipX[len1:(len1 - boundary1 + 1),(boundary2+1):len2] ## u1d2, quadrant II
-  hypermat[lenStrip1 + (boundary1+1):len1,1:boundary2] <- hypermat_flipX[(len1 - boundary1):1,1:boundary2] ## u1d2, quadrant IV
-
+  	max<-max(hypermat[is.finite(hypermat)])
+	hypermat[hypermat == Inf] <- max
+ hypermat[lenStrip1 + (boundary1+1):len1,1:boundary2] <- hypermat_flipX[(len1 - boundary1):1,1:boundary2] ## u1d2, quadrant IV
+	max<-max(hypermat[is.finite(hypermat)])
+	hypermat[hypermat == Inf] <- max
 
   if (log10.ind){
   	hypermat <- hypermat * log10(exp(1))

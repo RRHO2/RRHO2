c7c04f87e2997a3b287f7831c9adebde6d545b1f
KellyCahill <kmc152@pitt.edu>
2017-07-27 20:48:28 -0400

    Update RRHO2.R

--- R/RRHO2.R
+++ R/RRHO2.R
@@ -74,8 +74,10 @@ RRHO2 <- function (list1, list2, stepsize = defaultStepSize(list1, list2),
   hypermat <- matrix(NA,nrow=nrow(hypermat_normal) + lenStrip1,ncol=ncol(hypermat_normal) + lenStrip2)
   hypermat[1:boundary1,1:boundary2] <- hypermat_normal[1:boundary1,1:boundary2] ## u1u2, quadrant III
 	Q3<-hypermat[1:boundary1,1:boundary2]
-	min<-min(Q3[is.finite(Q3)])
-	Q3[Q3 == -Inf] <-min
+	#min<-min(Q3[is.finite(Q3)])
+	#Q3[Q3 == -Inf] <-min
+	max<-max(Q3[is.finite(Q3)])
+	Q3[Q3 == Inf] <-max
         hypermat[1:boundary1,1:boundary2]<-Q3
 
   hypermat[lenStrip1 + (boundary1+1):len1,lenStrip2 + (boundary2+1):len2] <- hypermat_normal[(boundary1+1):len1,(boundary2+1):len2] ## d1d2, quadrant I
@@ -92,8 +94,10 @@ RRHO2 <- function (list1, list2, stepsize = defaultStepSize(list1, list2),
 	
  hypermat[lenStrip1 + (boundary1+1):len1,1:boundary2] <- hypermat_flipX[(len1 - boundary1):1,1:boundary2] ## u1d2, quadrant IV
 	Q4<-hypermat[lenStrip1 + (boundary1+1):len1,1:boundary2]
-	max<-max(Q4[is.finite(Q4)])
-	Q4[Q4 == Inf] <- max
+	#max<-max(Q4[is.finite(Q4)])
+	#Q4[Q4 == Inf] <- max
+	min<-min(Q4[is.finite(Q4)])
+	Q4[Q4 == -Inf] <- min
 	hypermat[lenStrip1 + (boundary1+1):len1,1:boundary2] <-Q4
 
 	

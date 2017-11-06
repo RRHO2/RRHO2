3e71445a2de5eee16967d7358f1c7c1524facb9e
KellyCahill <kmc152@pitt.edu>
2017-11-06 11:39:15 -0500

    Update RRHO2.R

--- R/RRHO2.R
+++ R/RRHO2.R
@@ -174,8 +174,9 @@ RRHO2 <- function (list1, list2, stepsize = defaultStepSize(list1, list2),
                                            na.rm = TRUE), max = max(hypermat[finite.ind],
                                                                     na.rm = TRUE), nticks = 6, title = "-log(P-value)")
       dev.off()   
-	      ## maximum
-      .filename <- paste("RRHOMap_markH_fixMax_combined_", labels[1], "_VS_",
+	    
+	  ## maximum
+      .filename <- paste("RRHOMap_fixMax_combined_", labels[1], "_VS_",
                          labels[2], ".tiff", sep = "")
       tiff(filename = paste(outputdir, .filename, sep = "/"),
            width = 8, height = 8, units = "in", 
@@ -188,23 +189,16 @@ RRHO2 <- function (list1, list2, stepsize = defaultStepSize(list1, list2),
             axes = FALSE, main = "Rank Rank Hypergeometric Overlap Map")
 	  segments(x0 = boundary1/len1 ,x1 = boundary1 /len1 ,y0 = -0.2,y1 = 1.2,lwd=4,col='white')
 	  segments(x0 = -0.2,x1 = 1.2,y0 = boundary2/len2,y1 = boundary2/len2,lwd=4,col='white')	  
-	  x1.dd <- (maxind.dd[1] - 1)/(nrow(hypermat) - 1)
-	  x2.dd <- (maxind.dd[2] - 1)/(ncol(hypermat) - 1)
-	  points(x1.dd,x2.dd,pch=18,cex=4)	  	  
-	  x1.uu <- (maxind.uu[1] - 1)/(nrow(hypermat) - 1)
-	  x2.uu <- (maxind.uu[2] - 1)/(ncol(hypermat) - 1)
-	  points(x1.uu,x2.uu,pch=18,cex=4)	  	  
-	  x1.ud <- (maxind.ud[1] - 1)/(nrow(hypermat) - 1)
-	  x2.ud <- (maxind.ud[2] - 1)/(ncol(hypermat) - 1)
-	  points(x1.ud,x2.ud,pch=18,cex=4)	  	  
-	  x1.du <- (maxind.du[1] - 1)/(nrow(hypermat) - 1)
-	  x2.du <- (maxind.du[2] - 1)/(ncol(hypermat) - 1)
-	  points(x1.du,x2.du,pch=18,cex=4)	  	  
       mtext(labels[2], 2, 0.5)
       mtext(labels[1], 1, 0.5)
       finite.ind <- is.finite(hypermat)
       color.bar(jet.colors(101), min = 0, max = maximum, nticks = 6, title = "-log(P-value)")
       dev.off()
+  
+	
+	    
+	    
+	    
 
     })
   }

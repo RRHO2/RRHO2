c59e55b3eb84c0abeb53151202a844daaf514855
KellyCahill <kmc152@pitt.edu>
2018-01-09 14:25:25 -0500

    Update RRHO2.R

--- R/RRHO2.R
+++ R/RRHO2.R
@@ -57,7 +57,7 @@ RRHO2 <- function (list1, list2, stepsize = defaultStepSize(list1, list2),
   .hypermat_flipX <- numericListOverlap(rev(list1[, 1]), list2[, 1], stepsize, method=method, alternative = alternative)
 	
 	####Add options for old method#####
-	if(alternative = "two.sided" | alternative = "enrichment"){
+	if(alternative == "two.sided" | alternative == "enrichment"){
 		
 if(log10.ind) hypermat<- hypermat *log10(exp(1))  
     

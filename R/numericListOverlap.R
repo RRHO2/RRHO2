dacb9ea54e9513ada5610dc9e48c7f2f0606a8ff
KellyCahill <kmc152@pitt.edu>
2018-01-15 16:11:49 -0500

    Update numericListOverlap.R

--- R/numericListOverlap.R
+++ R/numericListOverlap.R
@@ -23,8 +23,8 @@ numericListOverlap<- function(sample1, sample2, stepsize, method="hyper", altern
              }
              log.pval<- -log(phyper(q=lower+tol, m=a, n=n-a+1, k=b, lower.tail=TRUE) +
                  phyper(q= upper-tol, m=a, n=n-a+1, k=b, lower.tail=FALSE))                               
-           #if(as.numeric(log.pval)==Inf){
-            # log.pval<-0}
+           if(as.numeric(log.pval)==Inf){
+             log.pval<-0}
              },
           split={
               log.pval<- -phyper(q=count-1, m=a, n=n-a+1, k=b, lower.tail=FALSE, log.p=TRUE)    

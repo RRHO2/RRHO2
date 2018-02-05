f4c2e1734fbf7879edcc881db2b231f4a8f5ce66
KellyCahill <kmc152@pitt.edu>
2018-02-05 12:15:39 -0500

    Update RRHO2.R

--- R/RRHO2.R
+++ R/RRHO2.R
@@ -223,6 +223,9 @@ RRHO2 <- function (list1, list2, stepsize = defaultStepSize(list1, list2),
 		
 } else {
 	#####Return to split method###
+ .hypermat_normal<- numericListOverlap(list1[, 1], list2[, 1], stepsize, method=method, alternative = alternative)
+  hypermat_normal<- .hypermat_normal$log.pval
+ 
  .hypermat_flipX <- numericListOverlap(rev(list1[, 1]), list2[, 1], stepsize, method=method, alternative = alternative)
   hypermat_flipX <- .hypermat_flipX$log.pval
   hypermat_flipX2 <- hypermat_flipX[nrow(hypermat_flipX):1,]

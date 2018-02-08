a2497dc5212683de2b8e4c6d9a89a62e27e1c616
KellyCahill <kmc152@pitt.edu>
2018-02-08 11:31:10 -0500

    Update numericListOverlap.R

--- R/numericListOverlap.R
+++ R/numericListOverlap.R
@@ -24,7 +24,7 @@ numericListOverlap<- function(sample1, sample2, stepsize, method="hyper", altern
              log.pval<- -log(phyper(q=lower+tol, m=a, n=n-a+1, k=b, lower.tail=TRUE) +
                  phyper(q= upper-tol, m=a, n=n-a+1, k=b, lower.tail=FALSE))   
              # max<-log.pval[is.finite(log.pval)==TRUE]
-            log.pval[!is.finite(log.pval)]<-500
+            log.pval[!is.finite(log.pval)]<-720
              if(as.numeric(log.pval*signs)<0){
                signs<-1} #need to fix later 
              },

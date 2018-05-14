2efaa471bfba9a5b8f1559a1b767ca809963b92b
KellyCahill <kmc152@pitt.edu>
2018-05-14 12:39:00 -0400

    Update numericListOverlap.R

--- R/numericListOverlap.R
+++ R/numericListOverlap.R
@@ -49,9 +49,8 @@ numericListOverlap<- function(sample1, sample2, stepsize, method="hyper", altern
       Odds <- 1
       }
     logOdds <- log(abs(Odds))*sign(Odds)
-    logOdds[Odds == 0]<- maximum 
-                #logOdds[logOdds<0]<- -maximum
-    
+    #logOdds[Odds == 0]<- maximum 
+    #logOdds[logOdds<0]<- -maximum
     #logOdds[!is.finite(logOdds)] <- sign(logOdds[!is.finite(logOdds)]) * 100
     signs<- 1L
     
